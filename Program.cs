625ae210071746ffd1c3da0ec850e59c345b32c3
Antonio Junior <antoniocarlosjuninhojuca@gmail.com>
2021-12-01 15:10:18 -0300

    AtivManipulaçãoDeString

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TratamentoText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var resultado = Maisculo("Desenvolvimento de Sistemas - ams");
+
+          
+           Console.WriteLine (resultado);
+            
+        }
+
+      
+
+        public static string Maisculo(string str)
+        {
+            return str.Length < 4 ? str.ToUpper() : str.Remove(str.Length  -3 ) + str.Substring(str.Length - 3) .ToUpper();
+        }
+
+    }
+
+}
